716a5fd8263005e68987665397492b61f26c95cf
Andy Evans <aevans@emlpayments.com>
2018-07-11 09:30:59 -0500

    Add project files.

---
+++ KCDC_AdvancedMessaging/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KCDC_AdvancedMessaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
